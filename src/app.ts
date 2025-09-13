82144e5dfd46a361ec670131618f9f168300a44d
parvezahmed2 <parvezahmed9432@gmail.com>
2025-09-13 14:16:44 +0600

    part-1 done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-import  express, { Request, Response }  from "express";
+import  express, {  Request, Response }  from "express";
+import { router } from "./app/routes";
+ 
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+ 
+ 
 const app = express()
+app.use(express.json())
+app.use("/api/v1", router)
+// app.use(cors())
+
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome to Tour Management System Basckend",
     })
 })
+
+
+// global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+app.use(globalErrorHandler)
+//Route Not Found
+app.use(notFound)
+
 
 
 export default app
